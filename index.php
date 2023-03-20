a15dad4ae229651edf0b10476e77300b2302274a
Debora Cocchi <108935821+DeboraCocchi@users.noreply.github.com>
2023-03-20 15:49:20 +0100

    [first-commit]bg

--- index.php
+++ index.php
@@ -5,11 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles/style.css">
     <title>Edusogno</title>
 </head>
 
 <body>
-
+    
+    <!-- <div class="bg"> -->
+        <img src="assets/img/Vector 5.png" alt="vector5" id="vector-5">
+        <img src="assets/img/Vector 4.png" alt="vector4" id="vector-4">
+        <img src="assets/img/Vector 1.png" alt="vector1" id="vector-1">
+        
+        <img src="assets/img/rocket.svg" alt="rocket" id="rocket">
+        <div id="circle-1" class="circle"></div>
+        <div id="circle-2" class="circle"></div>
+    <!-- </div> -->
+    <header>
+        <nav>
+            <img src="assets/img/Logo-edusogno.png" alt="logo">
+        </nav>
+    </header>
 </body>
 
 </html>
